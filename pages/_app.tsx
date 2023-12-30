d6c418a96bd67c6ce8e1551bb2144832ac58aa11
Kungpao <filip.yao@outlook.cz>
2023-12-30 16:37:35 +0100

    0.0.5 Fix,MOOORE FIX analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
-import type { AppProps } from 'next/app';
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 import { SpeedInsights } from '@vercel/speed-insights/next';
- 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <>
+    	<Component {...pageProps} />
       <SpeedInsights />
-    </>
-  );
+      </>
 }
- 
-export default MyApp;
+
