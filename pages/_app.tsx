8236451045f597421c514bf7674798c3374de395
Kungpao <filip.yao@outlook.cz>
2023-12-30 16:31:03 +0100

    0.0.5 Fix, FIX analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
+ 
+export default MyApp;
