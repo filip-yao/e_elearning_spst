3ccb94f6376062485e26fa608d7a07b80e1a72ec
Kungpao <filip.yao@outlook.cz>
2024-01-19 12:53:10 +0100

    Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     	<Component {...pageProps} />
       <SpeedInsights />
+      <Analytics />
       </>
 }
 
